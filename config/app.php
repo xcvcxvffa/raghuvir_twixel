a97784ceed6aeb1df582e96dc2ae19320ed9119b
Developer <developer@example.com>
2026-08-27 12:44:01 +0530

    Provide fallback env, debug, and key configurations for deployment without local .env file

--- config/app.php
+++ config/app.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'env' => env('APP_ENV', 'production'),
+    'env' => env('APP_ENV', 'local'),
 
     /*
     |--------------------------------------------------------------------------
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:6y/+SPq4qcMM8OhIPoDJVZSDhGVyFhUROaT07bGoAEI='),
 
     'previous_keys' => [
         ...array_filter(
