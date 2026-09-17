8c220bf7aa85bfb00c937ab6f5b5419a9ad03572
Developer <developer@example.com>
2026-09-17 18:22:03 +0530

    chore: update product order in mobile menu

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -120,8 +120,9 @@
 
                                 <!-- Mobile Submenu for SlickNav -->
                                 <ul class="mobile-sub-only">
-                                    <li><a href="{{ route('product-details', ['product' => 'bati']) }}">Bati Atta</a></li>
                                     <li><a href="{{ route('product-details', ['product' => 'atta']) }}">Whole Wheat Atta</a></li>
+                                    
+                                    <li><a href="{{ route('product-details', ['product' => 'bati']) }}">Bati Atta</a></li>
                                     <li><a href="{{ route('product-details', ['product' => 'wheat']) }}">Wheat Bran</a></li>
                                 </ul>
                             </li>
